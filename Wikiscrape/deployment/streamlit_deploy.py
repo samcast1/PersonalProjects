ea259920edad4b81ba46aa0e5612d6b660739b68
samcast1 <castillosam27@gmail.com>
2024-08-13 13:39:36 -0400

    Update streamlit_deploy.py

--- Wikiscrape/deployment/streamlit_deploy.py
+++ Wikiscrape/deployment/streamlit_deploy.py
@@ -86,7 +86,7 @@ event_placeholder = ''
 if event_type == 'event':
     event_placeholder = "Event: "
 elif event_type == 'birth':
-    event_placeholder = "Birth: "
+    event_placeholder = '''**Birth: **'''
 elif event_type == 'death':
     event_placeholder = "Death: "
 
@@ -111,7 +111,7 @@ if st.button("Search"):
                 st.subheader("Result")
                 st.write(f"**Date:** {result['date']}")
                 st.write(f"**Year:** {result['year']}")
-                st.markdown(f"**{event_placeholder}** {result['text']}")
+                st.write(f"**{event_placeholder}** {result['text']}")
                 
                 st.subheader("Learn more below!")
                 for i in range(1, 21):
